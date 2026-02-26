d57e7df495a37b263e715afdb9edacf2251da36c
Choko29 <saba.chokoshvili@ug.edu.ge>
2026-02-27 01:26:30 +0400

    Fix CORS policy to allow frontend connection

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,17 +1,20 @@
 <?php
 
-if (isset($_GET['setup_db'])) {
-    require_once __DIR__ . '/../import_data.php';
-    exit;
-}
-
+// 1. CORS Headers
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
+// 2. Handle preflight OPTIONS request
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
+}
+
+// 3. Database setup route
+if (isset($_GET['setup_db'])) {
+    require_once __DIR__ . '/../import_data.php';
+    exit;
 }
 
 require_once __DIR__ . '/../vendor/autoload.php';
@@ -27,11 +30,9 @@ $routeInfo = $dispatcher->dispatch(
 
 switch ($routeInfo[0]) {
     case FastRoute\Dispatcher::NOT_FOUND:
-        
         break;
     case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
         $allowedMethods = $routeInfo[1];
-        
         break;
     case FastRoute\Dispatcher::FOUND:
         $handler = $routeInfo[1];
